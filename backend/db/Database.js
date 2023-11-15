c0fcec47dbed43b91b111b2d87aef2202b4667e8
HoangNam2409 <namzinv123@gmail.com>
2023-11-15 20:58:53 +0700

    setup backend

---
+++ backend/db/Database.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_URL).then((data) => console.log(`MongoDB connected with server: ${data.connection.host}`))
+}
+
+export default connectDatabase;
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+
+// Handling uncaught exception
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting down the server for handling uncaught exceptionX");
+});
+
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    dotenv.config({
+        path: "config/.env",
+    });
+}
+
+// Create server
+const server = app.listen(process.env.PORT, () =>
+    console.log(`Server is running on http://localhost:${process.env.PORT}`)
+);
+
+// Unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+    console.log(`Shutting down the server for ${err.message}`);
+    console.log("Shutting down the server for unhandled promise rejection");
+
+    server.close(() => {
+        process.exit(1);
+    });
+});
