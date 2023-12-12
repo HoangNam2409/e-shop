d6c3b15dc46a0952dcb87670ace15f5a51fe7ecc
HoangNam2409 <namzinv123@gmail.com>
2023-12-13 02:00:26 +0700

    Complete Sponsored & Footer Frontend

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,8 @@ import Categories from "../components/Route/Categories/Categories.jsx";
 import BestDeals from "../components/Route/BestDeals/BestDeals.jsx";
 import Events from "../components/Events/Events.jsx";
 import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct.jsx";
+import Sponsored from "../components/Route/Sponsored.jsx";
+import Footer from "../components/Layout/Footer.jsx";
 
 export default function HomePage() {
     return (
@@ -12,9 +14,11 @@ export default function HomePage() {
             <Header activeHeading={1} />
             <Hero />
             <Categories />
-            <BestDeals />
+            <BestDeals /> d
             <Events />
             <FeaturedProduct />
+            <Sponsored />
+            <Footer />
         </div>
     );
 }
